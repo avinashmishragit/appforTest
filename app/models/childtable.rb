8500cf762886f8aaec6b9c2104ce7412f955c818
inncoentavi <mishra.avinash35@gmail.com>
2013-01-13 19:34:14 +0530

    parent Child relationship established

--- app/models/childtable.rb
+++ app/models/childtable.rb
@@ -1,3 +1,4 @@
 class Childtable < ActiveRecord::Base
   attr_accessible :description_of_Table, :name, :other_detail, :table_id
+  belongs_to:table
 end
--- app/models/table.rb
+++ app/models/table.rb
@@ -1,3 +1,4 @@
 class Table < ActiveRecord::Base
   attr_accessible :desc, :table_child_id, :table_name
+  has_many :childtables ,:dependent => :destroy
 end
