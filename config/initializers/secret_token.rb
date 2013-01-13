9d206efd1e528e88526f6d2867dd3f2d82fbd81a
ubuntu <ubuntu@ubuntu.(none)>
2013-01-13 13:36:28 +0530

    empty poroject created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appfortest::Application.config.secret_token = '3b673ee0b33bd3dec830c0ae108734598e72f9ae049ef2886b2e61552aa081f49860d65f932d49613e20bf3dd003cd1f0be4fd799f1c326f86e4f66a001fd6b7'
